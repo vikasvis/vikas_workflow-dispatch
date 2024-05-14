59ce83948df482265ddd4f296e8469c2310152ae
Frédéric Biesse <f.biesse@alptis.fr>
2024-05-14 10:30:26 +0200

    fix: lint

--- src/debug.ts
+++ src/debug.ts
@@ -4,10 +4,10 @@ export function debug(title: string, content: any) {
   if (core.isDebug()) {
     core.info(`::group::${title}`)
     try {
-      core.debug(JSON.stringify(content, null, 3));      
+      core.debug(JSON.stringify(content, null, 3))
     } catch(e) {
       core.debug(`Failed to serialize object, trying toString. Cause: ${e}`)
-      core.debug(content?.toString());    
+      core.debug(content?.toString())
     }
     core.info('::endgroup::')
   }
